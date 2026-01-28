e40d0905d5f48837b56eced5984e2d35e528eee1
Erik <65229112+EjayGit@users.noreply.github.com>
2026-01-28 20:48:43 +0000

    Update URL and enhance keyword matching in analyseJS

--- analyseJS.py
+++ analyseJS.py
@@ -4,24 +4,26 @@ from urllib.parse import urljoin
 import re
 
 # Identify website for analysis.
-url = 'https://erikjclark.co.uk'
+url = 'https://week-08-iota.vercel.app/'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'lxml')
 
 # Extract the JS/JSX file using bs4.
 scripts = soup.find_all('script')
 for script in scripts:
-    print(script)
+    # print(script)
     if script.get('src'):
         src = urljoin(url,script['src'])
-        print(src)
+        # print(src)
 jsResponse = requests.get(src)
 # print(jsResponse.text)
 
 # Find keywords such as 'keyup' and 'keydown', or letters encapsulated by ''.
 # test = 'fetc the "w"ater"'
-pattern = r'(keyDown|keyUp|keyPress|fetch|endpoint|((\'|\")[a-zA-Z](\'|\")))'
-if re.search(pattern, test):
-    print('Match')
+pattern = r'(keyDown|keyUp|keyPress|onkeydown|onkeyup|onkeypress)'
+matches = ''
+matches = re.findall(pattern, jsResponse.text)
+if matches:
+    print(matches)
 else:
     print('No match')
