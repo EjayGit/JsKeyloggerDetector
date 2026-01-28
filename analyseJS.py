746377da6b9727a6b6ae90c3f5c59b91a963cc32
Erik <65229112+EjayGit@users.noreply.github.com>
2026-01-28 20:51:06 +0000

    Refactor jsScanner function for website analysis

--- analyseJS.py
+++ analyseJS.py
@@ -3,27 +3,29 @@ import requests
 from urllib.parse import urljoin
 import re
 
-# Identify website for analysis.
-url = 'https://week-08-iota.vercel.app/'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'lxml')
+def jsScanner(url):
+    # Identify website for analysis.
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
 
-# Extract the JS/JSX file using bs4.
-scripts = soup.find_all('script')
-for script in scripts:
-    # print(script)
-    if script.get('src'):
-        src = urljoin(url,script['src'])
-        # print(src)
-jsResponse = requests.get(src)
-# print(jsResponse.text)
+    # Extract the JS/JSX file using bs4.
+    scripts = soup.find_all('script')
+    for script in scripts:
+        # print(script)
+        if script.get('src'):
+            src = urljoin(url,script['src'])
+            # print(src)
+    jsResponse = requests.get(src)
+    # print(jsResponse.text)
 
-# Find keywords such as 'keyup' and 'keydown', or letters encapsulated by ''.
-# test = 'fetc the "w"ater"'
-pattern = r'(keyDown|keyUp|keyPress|onkeydown|onkeyup|onkeypress)'
-matches = ''
-matches = re.findall(pattern, jsResponse.text)
-if matches:
-    print(matches)
-else:
-    print('No match')
+    # Find keywords such as 'keyup' and 'keydown', or letters encapsulated by ''.
+    # test = 'fetc the "w"ater"'
+    pattern = r'(keyDown|keyUp|keyPress|onkeydown|onkeyup|onkeypress)'
+    matches = ''
+    matches = re.findall(pattern, jsResponse.text)
+    if matches:
+        print(matches)
+    else:
+        print('No match')
+
+jsScanner('https://week-08-iota.vercel.app/')
